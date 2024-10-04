073d534de49f7b82cd2eda3a56073f8d07440dc5
fadyl <ganiyufadi@gmail.com>
2024-10-04 15:12:48 +0100

    Initial commit

---
+++ Text to Morse.py
@@ -0,0 +1,24 @@
+morse_dict = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.',
+    'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
+    'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
+    'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..',
+    '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-',
+    '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.',
+    ' ': ' '  # Space character
+}
+
+string= input("Enter the word you'd like to convert:")
+
+def text_to_morse():
+    text = string.upper()
+    morse=[]
+    for char in text:
+        if char in morse_dict:
+            morse.append(morse_dict[char])
+        else:
+            morse.append(morse_dict[''])
+    return morse
+
+print(f'{string} : {text_to_morse()}')
